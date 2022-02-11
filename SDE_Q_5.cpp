33bb68ee87648863e17d59f9a029e2174f60fa11
Vivek <vivekyadav6838@gmail.com>
2022-02-11 13:23:42 +0530

    commit

---
+++ SDE_Q_5.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    unor
+    if()
+}
--- template.cpp
+++ template.cpp
@@ -1,13 +1,18 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int t;
-    cin>>t;
-
-    while(t--)
-    {
-        cout<<"Jai Shree Ram"<<endl;
-
-    }
-}
+unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        priority_queue<pair<int,int>>pq;
+        for(auto it=mp.begin();it!=mp.end();it++)
+        {
+            //pair -> frequency,element
+            pq.push({it->second,it->first});
+        }
+        vector<int>v;
+        while(k--)
+        {
+            v.push_back(pq.top().second);
+            pq.pop();
+        }
+        return v;
