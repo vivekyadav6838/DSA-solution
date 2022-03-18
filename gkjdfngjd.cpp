0a5c3e24ebadfb7b91f90e5c78afd42086848f77
Vivek <vivekyadav6838@gmail.com>
2022-03-18 19:52:51 +0530

    commit

---
+++ gkjdfngjd.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define for(i,n) for (int i = 0; i < n; ++i)
+#define int long long int
+#define arr(n) int arr[n]
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while(t--){}
+    return 0;
+}
